bd86614cd4541358335ebdf695ff374a187351bd
Singh22456 <brahmjotkhalsa@gmail.com>
2025-07-28 12:48:04 +0530

    Initial Commit

---
+++ LayoverBudddy/LayoverBudddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoverBudddyApp.swift
+//  LayoverBudddy
+//
+//  Created by Brahmjot Singh Tatla on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoverBudddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
